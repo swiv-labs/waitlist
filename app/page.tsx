5c20b7331d4b61065ddc95f4dd148b823ff7190e
David-Horjet <davidojetola956@gmail.com>
2026-02-22 15:38:37 +0100

    working on the ui

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,11 @@ import {
   EnvelopeIcon,
 } from "@heroicons/react/24/outline";
 import { FaXTwitter } from "react-icons/fa6";
+import Link from "next/link";
+import logo from "../../../public/images/logos/swiv-white-nobg.png"
+import blogo from "../../../public/images/logos/swiv-black-nobg.png"
+import mlogo from "../../../public/images/logos/icon.png"
+import Image from "next/image";
 
 export default function Home() {
   const [username, setUsername] = useState("");
@@ -27,11 +32,39 @@ export default function Home() {
   return (
     <div className="relative min-h-screen bg-[#05070d] text-white overflow-hidden">
       {/* Subtle Grid Background */}
-      <div className="absolute inset-0 bg-[linear-gradient(rgba(255,255,255,0.03)_1px,transparent_1px),linear-gradient(to_right,rgba(255,255,255,0.03)_1px,transparent_1px)] bg-[size:60px_60px]" />
+      <div className="absolute inset-0 bg-[linear-gradient(rgba(255,255,255,0.03)_1px,transparent_1px),linear-gradient(to_right,rgba(255,255,255,0.03)_1px,transparent_1px)] bg-size-[60px_60px]" />
 
       {/* Soft radial glow */}
       <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(88,101,242,0.15),transparent_70%)]" />
 
+      <header className="fixed z-50 px-2 md:px-4 top-0 left-0 right-0 w-full flex justify-center bg-background/10 backdrop-blur-xl border-b border-border pointer-events-none">
+        <nav
+          className={`flex items-center justify-between px-0 sm-px-2 py-2 shadow-lg pointer-events-auto
+          transition-all duration-500 ease-in-out
+          ${
+            // scrolled
+            "w-full max-w-7xl rounded-none shadow-md translate-y-0"
+            // "w-[95%] max-w-6xl mt-6 rounded-2xl shadow-xl"
+            }`}
+        >
+          <div className="flex items-center gap-8">
+            {/* Logo */}
+            <Link href={"/"} className="flex items-center">
+              <Image className="w-16 hidden dark:md:flex" src={logo} alt="logo" />
+              <Image className="w-16 hidden md:flex dark:hidden" src={blogo} alt="logo" />
+              <Image className="w-8 md:hidden flex" src={mlogo} alt="mlogo" />
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <button
+              className="relative px-6 py-2 text-sm font-semibold text-white bg-linear-to-r from-primary to-accent rounded-xl transition-smooth hover:shadow-lg hover:shadow-primary/50 hover:scale-105 disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Join Waitlist
+            </button>
+          </div>
+        </nav>
+      </header>
       <div className="relative z-10 flex min-h-screen flex-col items-center justify-center px-6">
         {/* Top Label */}
         <p className="text-xs tracking-widest text-gray-400 mb-16">
